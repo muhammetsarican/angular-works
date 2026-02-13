6f0760eca26179a00d801093d5ee251b37b50857
muhammetsarican <muhammetsarican@gmail.com>
2026-02-13 18:01:11 +0300

    feat(todo added)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,4 +9,8 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./auth/login').then((m) => m.Login)
   },
+  {
+    path: 'todo',
+    loadComponent: () => import('./pages/todo/todo').then((m) => m.Todo)
+  },
 ];
