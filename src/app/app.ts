539c57f7630d580eac53d5fca4517372ba10740a
muhammetsarican <muhammetsarican@gmail.com>
2026-02-13 15:57:47 +0300

    style(app design improved by ai)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { NgClass } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, NgClass, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   titleForFun = 'angular-works for fun'
+  inputModel = ''
+  gender = ''
 
   constructor(private router: Router) { }
 
   navigateToLogin() {
     this.router.navigate(['/login']);
+  }
+
+  onNameChange(event: Event) {
+    this.titleForFun = (event.target as HTMLInputElement).value;
   }
 }
